62e32418a45d97be949e76ef8a8e3f636153ee1f
Your Name <noynosrati@gmail.com>
2023-04-18 15:12:23 +0300

    15:11

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async(req,res) => {
-  res.json({msg:"Express homepage work"});
+  res.json({msg:"Express homepage work 15:11"});
 })
 
 module.exports = router;
